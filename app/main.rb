7f649b325f73e72d6801f0847eec666d46af1c04
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-13 07:50:05 -0300

    pass 1st stage

--- app/main.rb
+++ app/main.rb
@@ -7,8 +7,8 @@ if command == ".dbinfo"
     puts "Logs from your program will appear here"
 
     # Uncomment this to pass the first stage
-    # database_file.seek(16)  # Skip the first 16 bytes of the header
-    # page_size = database_file.read(2).unpack("n")[0]
-    # puts "database page size: #{page_size}"
+    database_file.seek(16)  # Skip the first 16 bytes of the header
+    page_size = database_file.read(2).unpack("n")[0]
+    puts "database page size: #{page_size}"
   end
 end
